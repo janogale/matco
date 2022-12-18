473d7cbcd0c24d778484d5f1926e0c23a41561b6
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2022-12-18 10:33:39 +0300

    Add flowbite tailwind ui lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+import { Alert, Navbar } from "flowbite-react";
 export default function Home() {
   return (
-    <main className="flex justify-center items-center bg-green-500 h-screen w-screen">
-      <h1 className=" text-3xl font-bold underline">Hello, Next.js!</h1>
+    <main className="flex flex-col justify-center  ">
+    
     </main>
   );
 }
