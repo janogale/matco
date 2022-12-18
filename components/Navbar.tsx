4c4e482d112222ae9505950b2571eaa0853b5f90
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2022-12-18 12:48:33 +0300

    Add logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,7 @@ export default function NavbarTop() {
   return (
     <Navbar fluid={true} rounded={true}>
       <Navbar.Brand href="https://flowbite.com/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
+        <img src="logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           MATCO
         </span>
