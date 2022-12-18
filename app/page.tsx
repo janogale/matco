ca4920f7cecbdde1b4dac0a3196975dd8a5abf62
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2022-12-18 09:21:34 +0300

    Setup tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="flex justify-center items-center bg-green-500 h-screen w-screen">
+      <h1 className=" text-3xl font-bold underline">Hello, Next.js!</h1>
     </main>
   );
 }
