94d28639fe0b5af50dcaea080767edfaaa60d279
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2022-12-19 10:22:27 +0300

    Minor restyle

--- components/ui/Banner.tsx
+++ components/ui/Banner.tsx
@@ -1,11 +1,11 @@
-export default function Banner({ size = "text-4xl", text1 = "", text2 = "" }) {
+export default function Banner({ size = "text-2xl", text1 = "", text2 = "" }) {
   return (
-    <div className="text-center my-8">
+    <div className="text-center py-12 ">
       <h1
-        className={`${size} font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl`}
+        className={`${size} font-bold tracking-tight text-sky-600 sm:text-4xl md:text-5xl`}
       >
-        <span className="block">{text1}</span>
-        <span className="block text-orange-600">{text2}</span>
+        <span className="block mb-2">{text1}</span>
+        <span className="block text-green-600">{text2}</span>
       </h1>
     </div>
   );
