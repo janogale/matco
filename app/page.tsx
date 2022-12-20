e677d624c02d5f637dc112a72ba43262cc94b18c
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2022-12-20 16:45:36 +0300

    Minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import HomeCarousel from "../components/HomeCarousel";
 import Navbar from "../components/Navbar";
 import TabsCars from "../components/TabsCars";
 import Container from "../components/ui/Container";
+import Heading from "../components/ui/Heading";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <HomeCarousel />
       <Container>
         <Banner text1="Explore the Affordable" text2="New Models" />
+        <Heading>Car Models</Heading>
         <TabsCars />
       </Container>
     </main>
