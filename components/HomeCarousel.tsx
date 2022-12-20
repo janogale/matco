54a21a5be2259fb00f3d44fa4e934f46d35ca5a3
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2022-12-20 17:24:02 +0300

    add jac motors logo

--- components/HomeCarousel.tsx
+++ components/HomeCarousel.tsx
@@ -11,6 +11,13 @@ function HomeCarousel() {
             className="absolute h-6 sm:h-12 z-50 bottom-16 left-4"
           />
         </div>
+        <div className="relative h-full w-full bg-no-repeat bg-cover bg-center bg-fixed bg-[url('/images/jac1.webp')]">
+          <img
+            src="/images/jaclogo.png"
+            alt="..."
+            className="absolute h-6 sm:h-12 z-50 bottom-16 left-4"
+          />
+        </div>
         <div className="relative h-full w-full bg-no-repeat bg-cover bg-center bg-fixed bg-[url('/images/celerio.png')]">
           <img
             src="/images/celeriologo.png"
