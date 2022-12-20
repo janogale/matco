108a4ee43bcb211379ac3aa55cf28e1abbd38d61
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2022-12-20 16:27:54 +0300

    Create ui Tabs

--- components/TabsCars.tsx
+++ components/TabsCars.tsx
@@ -1,10 +1,46 @@
-import { Tabs } from "flowbite-react";
+import { Tabs, Card } from "flowbite-react";
+import CarList from "./CarList";
+import ImageCard from "./ui/ImageCard";
 
 export default function TabsCars() {
   return (
     <Tabs.Group aria-label="Tabs" style="underline" className="text-2xl">
-      <Tabs.Item className="text-2xl" title="All">
-        Profile content
+      <Tabs.Item title="All">
+        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4">
+          <ImageCard
+            img="/images/models/celerio/1.png"
+            logo="/images/celeriologo.png"
+            alt="Celerio"
+          />
+          <Card
+            imgSrc="/images/models/celerio/1.png"
+            className="hover:opacity-75"
+          ></Card>
+          <Card
+            imgSrc="/images/models/celerio/1.png"
+            className="hover:opacity-75"
+          ></Card>
+          <Card
+            imgSrc="/images/models/celerio/1.png"
+            className="hover:opacity-75"
+          ></Card>
+          <Card
+            imgSrc="/images/models/celerio/1.png"
+            className="hover:opacity-75"
+          ></Card>
+          <Card
+            imgSrc="/images/models/celerio/1.png"
+            className="hover:opacity-75"
+          ></Card>
+          <Card
+            imgSrc="/images/models/celerio/1.png"
+            className="hover:opacity-75"
+          ></Card>
+          <Card
+            imgSrc="/images/models/celerio/1.png"
+            className="hover:opacity-75"
+          ></Card>
+        </div>
       </Tabs.Item>
       <Tabs.Item active={true} title="Dashboard">
         All
