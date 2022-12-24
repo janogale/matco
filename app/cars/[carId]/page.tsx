1c3d54d67cc84f238e9e7a1e881422e12014eb4d
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2022-12-24 16:06:05 +0200

    feat: finish outside design

--- app/cars/[carId]/page.tsx
+++ app/cars/[carId]/page.tsx
@@ -62,7 +62,7 @@ export default function Page({ params }: any) {
               </div>
             </div>
             {/* interior section */}
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-2">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"></div>
               <div className="grid max-w-screen-lg gap-8 lg:grid-cols-2 sm:mx-auto">
                 <div className="flex flex-col justify-center">
@@ -102,17 +102,17 @@ export default function Page({ params }: any) {
                 </div>
                 <div className="grid grid-cols-2 gap-5">
                   <img
-                    className="object-cover w-full h-56 col-span-2 rounded shadow-lg"
+                    className="object-cover w-full h-56 col-span-2 rounded"
                     src="/images/models/jac/2.webp"
                     alt=""
                   />
                   <img
-                    className="object-cover w-full h-48 rounded shadow-lg"
+                    className="object-cover w-full h-48 rounded"
                     src="/images/models/jac/1.webp"
                     alt=""
                   />
                   <img
-                    className="object-cover w-full h-48 rounded shadow-lg"
+                    className="object-cover w-full h-48 rounded"
                     src="/images/models/jac/1.webp"
                     alt=""
                   />
@@ -121,105 +121,9 @@ export default function Page({ params }: any) {
             </div>
 
             {/* outside section */}
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-              <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-                <div className="bg-gray-300 p-2">
-                  <p className="text-xl font-bold text-gray-500">OUTSIDE</p>
-                </div>
-              </div>
+            <div className="px-4 py-6 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-4">
+              <div className="max-w-xl mb-2 md:mx-auto sm:text-center lg:max-w-2xl md:mb-2"></div>
               <div className="grid max-w-screen-lg gap-8 lg:grid-cols-2 sm:mx-auto">
-                <div className="flex flex-col justify-center">
-                  <div className="flex">
-                    <div className="mr-4">
-                      <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
-                        <svg
-                          className="w-8 h-8 text-deep-purple-accent-400"
-                          stroke="currentColor"
-                          viewBox="0 0 52 52"
-                        >
-                          <polygon
-                            strokeWidth="3"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            fill="none"
-                            points="29 13 14 29 25 29 23 39 38 23 27 23"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                    <div>
-                      <h6 className="mb-2 font-semibold leading-5">
-                        They urge you to put down
-                      </h6>
-                      <p className="text-sm text-gray-900">
-                        Skate ipsum dolor sit amet, alley oop vert mute-air
-                        Colby Carter flail 180 berm. Half-cab camel back ollie
-                        transition ledge Wes Humpston 1080.
-                      </p>
-                      <hr className="w-full my-6 border-gray-300" />
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <div className="mr-4">
-                      <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
-                        <svg
-                          className="w-8 h-8 text-deep-purple-accent-400"
-                          stroke="currentColor"
-                          viewBox="0 0 52 52"
-                        >
-                          <polygon
-                            strokeWidth="3"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            fill="none"
-                            points="29 13 14 29 25 29 23 39 38 23 27 23"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                    <div>
-                      <h6 className="mb-2 font-semibold leading-5">
-                        The powerless in a world
-                      </h6>
-                      <p className="text-sm text-gray-900">
-                        The first mate and his Skipper too will do their very
-                        best to make the others comfortable in their tropic
-                        island nest. Michael Knight a young loner.
-                      </p>
-                      <hr className="w-full my-6 border-gray-300" />
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <div className="mr-4">
-                      <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
-                        <svg
-                          className="w-8 h-8 text-deep-purple-accent-400"
-                          stroke="currentColor"
-                          viewBox="0 0 52 52"
-                        >
-                          <polygon
-                            strokeWidth="3"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            fill="none"
-                            points="29 13 14 29 25 29 23 39 38 23 27 23"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                    <div>
-                      <h6 className="mb-2 font-semibold leading-5">
-                        Tell them I hate them
-                      </h6>
-                      <p className="text-sm text-gray-900">
-                        Chase ball of string eat plants, meow, and throw up
-                        because I ate plants going to catch the red dot today
-                        going to catch the red dot today. I could pee on this if
-                        I had the energy.
-                      </p>
-                    </div>
-                  </div>
-                </div>
                 <div className="grid grid-cols-2 gap-5">
                   <img
                     className="object-cover w-full h-56 col-span-2 rounded shadow-lg"
@@ -236,6 +140,41 @@ export default function Page({ params }: any) {
                     src="/images/models/jac/1.webp"
                     alt=""
                   />
+                </div>
+                <div className="flex flex-col justify-center">
+                  <div className="bg-gray-300 p-2 w-full mb-4">
+                    <p className="text-xl font-bold text-gray-500">OUTSIDE</p>
+                  </div>
+                  <div className="flex">
+                    <div>
+                      <h6 className="mb-2 font-semibold leading-5">TITLE</h6>
+                      <p className="text-md text-gray-900 text-justify">
+                        Skate ipsum dolor sit amet, alley oop vert mute-air
+                        Colby Carter flail 180 berm. Half-cab camel back ollie
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                        Skate ipsum dolor sit amet, alley oop vert mute-air
+                        Colby Carter flail 180 berm. Half-cab camel back ollie
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                      </p>
+                      <hr className="w-full my-6 border-gray-300" />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
