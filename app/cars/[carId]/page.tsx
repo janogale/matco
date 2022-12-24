0addd82310f368ab5949ad34142a9f555d82ade0
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2022-12-24 13:50:09 +0200

    update gallery section responsive

--- app/cars/[carId]/page.tsx
+++ app/cars/[carId]/page.tsx
@@ -95,20 +95,20 @@ export default function Page({ params }: any) {
                 <div className="flex items-center justify-center -mx-4 lg:pl-8">
                   <div className="flex flex-col items-end px-3">
                     <img
-                      className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-                      src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                      className="object-contain  rounded shadow-lg h-28 sm:h-48 xl:h-40 w-32 sm:w-64 xl:w-96"
+                      src="/images/models/celerio/1.png"
                       alt=""
                     />
                     <img
-                      className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-                      src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                      className="object-contain w-40 h-40 rounded shadow-lg sm:h-32 xl:h-40 sm:w-64 xl:w-96"
+                      src="/images/models/baleno/1.jpeg"
                       alt=""
                     />
                   </div>
                   <div className="px-3">
                     <img
-                      className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-                      src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+                      className="object-contain w-58 h-48 rounded shadow-lg sm:h-64 xl:h-92 sm:w-64 xl:w-96"
+                      src="/images/models/ertiga/1.webp"
                       alt=""
                     />
                   </div>
@@ -119,38 +119,8 @@ export default function Page({ params }: any) {
             {/* second section */}
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-                <div>
-                  <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-                    Brand new
-                  </p>
-                </div>
                 <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-                  <span className="relative inline-block">
-                    <svg
-                      viewBox="0 0 52 24"
-                      fill="currentColor"
-                      className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-                    >
-                      <defs>
-                        <pattern
-                          id="3071a3ad-db4a-4cbe-ac9a-47850b69e4b7"
-                          x="0"
-                          y="0"
-                          width=".135"
-                          height=".30"
-                        >
-                          <circle cx="1" cy="1" r=".7" />
-                        </pattern>
-                      </defs>
-                      <rect
-                        fill="url(#3071a3ad-db4a-4cbe-ac9a-47850b69e4b7)"
-                        width="52"
-                        height="24"
-                      />
-                    </svg>
-                    <span className="relative">The</span>
-                  </span>{' '}
-                  quick, brown fox jumps over a lazy dog
+                  <span className="relative inline-block"></span> OUTSIDE CAR
                 </h2>
                 <p className="text-base text-gray-700 md:text-lg">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -263,7 +233,7 @@ export default function Page({ params }: any) {
                   />
                   <img
                     className="object-cover w-full h-48 rounded shadow-lg"
-                    src="/images/models/jac/2.webp"
+                    src="/images/models/jac/1.webp"
                     alt=""
                   />
                 </div>
