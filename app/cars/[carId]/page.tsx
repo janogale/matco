65be5378a12e603ca4707fe199f98071f3fcc0d9
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2022-12-24 15:56:23 +0200

    added interior section

--- app/cars/[carId]/page.tsx
+++ app/cars/[carId]/page.tsx
@@ -61,71 +61,71 @@ export default function Page({ params }: any) {
                 </a>
               </div>
             </div>
-
-            <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
-              <div className="w-full lg:w-1/2">
-                <div className="lg:max-w-lg">
-                  <h1 className="text-3xl font-semibold tracking-wide text-gray-800 dark:text-white lg:text-4xl">
-                    Find your Car you want
-                  </h1>
-                  <p className="mt-4 text-gray-600 text-justify">
-                    We work with the best remunated glasses dealers in US to
-                    find your new glasses. How to use sticky note for problem
-                    solving How to use sticky note for problem solving How to
-                    use sticky note for problem solving How to use sticky note
-                    for problem solving How to use sticky note for problem
-                    solving How to use sticky note for problem solving How to
-                    use sticky note for problem solving How to use sticky note
-                    for problem solving How to use sticky note for problem
-                    solving How to use sticky note for problem solving How to
-                    use sticky note for problem solving How to use sticky note
-                    for problem solving How to use sticky note for problem
-                    solving How to use sticky note for problem solving How to
-                    use sticky note for problem solving How to use sticky note
-                    for problem solving How to use sticky note for problem
-                    solving How to use sticky note for problem solving How to
-                    use sticky note for problem solving How to use sticky note
-                    for problem solving How to use sticky note for problem
-                    solving How to use sticky note for problem solving
-                  </p>
+            {/* interior section */}
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+              <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"></div>
+              <div className="grid max-w-screen-lg gap-8 lg:grid-cols-2 sm:mx-auto">
+                <div className="flex flex-col justify-center">
+                  <div className="bg-gray-300 p-2 w-full mb-4">
+                    <p className="text-xl font-bold text-gray-500">INTERIOR</p>
+                  </div>
+                  <div className="flex">
+                    <div>
+                      <h6 className="mb-2 font-semibold leading-5">TITLE</h6>
+                      <p className="text-md text-gray-900 text-justify">
+                        Skate ipsum dolor sit amet, alley oop vert mute-air
+                        Colby Carter flail 180 berm. Half-cab camel back ollie
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                        Skate ipsum dolor sit amet, alley oop vert mute-air
+                        Colby Carter flail 180 berm. Half-cab camel back ollie
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                      </p>
+                      <hr className="w-full my-6 border-gray-300" />
+                    </div>
+                  </div>
                 </div>
-              </div>
-
-              <div className="flex items-center justify-center w-full h-58 lg:w-1/3">
-                <div className="flex items-center justify-center -mx-4 lg:pl-8">
-                  <div className="flex flex-col items-end px-3">
-                    <img
-                      className="object-contain  rounded shadow-lg h-28 sm:h-48 xl:h-40 w-32 sm:w-64 xl:w-96"
-                      src="/images/models/celerio/1.png"
-                      alt=""
-                    />
-                    <img
-                      className="object-contain w-40 h-40 rounded shadow-lg sm:h-32 xl:h-40 sm:w-64 xl:w-96"
-                      src="/images/models/baleno/1.jpeg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="px-3">
-                    <img
-                      className="object-contain w-58 h-48 rounded shadow-lg sm:h-64 xl:h-92 sm:w-64 xl:w-96"
-                      src="/images/models/ertiga/1.webp"
-                      alt=""
-                    />
-                  </div>
+                <div className="grid grid-cols-2 gap-5">
+                  <img
+                    className="object-cover w-full h-56 col-span-2 rounded shadow-lg"
+                    src="/images/models/jac/2.webp"
+                    alt=""
+                  />
+                  <img
+                    className="object-cover w-full h-48 rounded shadow-lg"
+                    src="/images/models/jac/1.webp"
+                    alt=""
+                  />
+                  <img
+                    className="object-cover w-full h-48 rounded shadow-lg"
+                    src="/images/models/jac/1.webp"
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
 
-            {/* second section */}
+            {/* outside section */}
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-                  <span className="relative inline-block"></span> OUTSIDE CAR
-                </h2>
-                <p className="text-base text-gray-700 md:text-lg">
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque rem aperiam, eaque ipsa quae.
-                </p>
+                <div className="bg-gray-300 p-2">
+                  <p className="text-xl font-bold text-gray-500">OUTSIDE</p>
+                </div>
               </div>
               <div className="grid max-w-screen-lg gap-8 lg:grid-cols-2 sm:mx-auto">
                 <div className="flex flex-col justify-center">
