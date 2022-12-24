b6efe0e0ee89a7245cb63750f4e6eba471b1b912
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2022-12-24 16:07:21 +0200

    feat: added security section

--- app/cars/[carId]/page.tsx
+++ app/cars/[carId]/page.tsx
@@ -178,6 +178,65 @@ export default function Page({ params }: any) {
                 </div>
               </div>
             </div>
+
+            {/* security section */}
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-2">
+              <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"></div>
+              <div className="grid max-w-screen-lg gap-8 lg:grid-cols-2 sm:mx-auto">
+                <div className="flex flex-col justify-center">
+                  <div className="bg-gray-300 p-2 w-full mb-4">
+                    <p className="text-xl font-bold text-gray-500">SECURITY</p>
+                  </div>
+                  <div className="flex">
+                    <div>
+                      <h6 className="mb-2 font-semibold leading-5">TITLE</h6>
+                      <p className="text-md text-gray-900 text-justify">
+                        Skate ipsum dolor sit amet, alley oop vert mute-air
+                        Colby Carter flail 180 berm. Half-cab camel back ollie
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                        Skate ipsum dolor sit amet, alley oop vert mute-air
+                        Colby Carter flail 180 berm. Half-cab camel back ollie
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                        transition ledge Wes Humpston 1080. Skate ipsum dolor
+                        sit amet, alley oop vert mute-air Colby Carter flail 180
+                        berm. Half-cab camel back ollie transition ledge Wes
+                        Humpston 1080. Skate ipsum dolor sit amet, alley oop
+                        vert mute-air Colby Carter flail 180 berm. Half-cab
+                        camel back ollie transition ledge Wes Humpston 1080.
+                      </p>
+                      <hr className="w-full my-6 border-gray-300" />
+                    </div>
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 gap-5">
+                  <img
+                    className="object-cover w-full h-56 col-span-2 rounded"
+                    src="/images/models/jac/2.webp"
+                    alt=""
+                  />
+                  <img
+                    className="object-cover w-full h-48 rounded"
+                    src="/images/models/jac/1.webp"
+                    alt=""
+                  />
+                  <img
+                    className="object-cover w-full h-48 rounded"
+                    src="/images/models/jac/1.webp"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
