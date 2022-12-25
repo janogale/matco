a0a3f304c11f84aeb548369850df45623b637e2f
rimoza <ridwan.maxamed1@gmail.com>
2022-12-25 01:06:13 -0800

    Add mission and vision section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -24,7 +24,7 @@ export default function AboutPage() {
         <div className="md:max-w-screen-xl mx-auto px-5 xxl:px-0 text-gray-800">
           <div className="w-full space-y-10">
             <div className="content text-lg">
-              <p>
+              <p className="mb-5">
                 Al Rostamani Trading Company (ARTC), a member of Al Rostamani
                 Group which is one of the largest and oldest business
                 conglomerates in the United Arab Emirates. Founded in 1957 with
@@ -33,7 +33,7 @@ export default function AboutPage() {
                 with the guiding principles of commitment, care and vision with
                 genuine concern for the community.
               </p>
-              <p>
+              <p className="mb-5">
                 ARTC is the sole authorized distributor of Suzuki automobiles in
                 the UAE which is headquartered in Dubai, was founded in the year
                 2002 and is now well established and respected in the market.
@@ -41,7 +41,7 @@ export default function AboutPage() {
                 Service and Parts outlets able to serve its customers throughout
                 the country.
               </p>
-              <p>
+              <p className="mb-5">
                 Suzuki offers a wide range of product lines to satisfy the needs
                 of most new car buyers in terms of quality, safety and value for
                 money. It also efficiently caters to the transportation needs of
@@ -84,7 +84,7 @@ export default function AboutPage() {
                 />
                 <div className="absolute bottom-0 left-0 p-3">
                   <div className="text-white text-3xl text-shadow-overlay">
-                  PERFORMANCE
+                    PERFORMANCE
                   </div>
                 </div>
               </div>
@@ -96,15 +96,123 @@ export default function AboutPage() {
                 />
                 <div className="absolute bottom-0 left-0 p-3">
                   <div className="text-white text-3xl text-shadow-overlay">
-                  SUPPORT
+                    SUPPORT
                   </div>
                 </div>
               </div>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className="content text-lg">
+              <h2 className="text-3xl uppercase text-sky-600 font-bold">MISSION</h2>
+              <p>
+                Al Rostamani Group’s mission is to be recognized as a leading
+                successful regional conglomerate with diverse commercial
+                interests and core competencies in select sectors.
+              </p>
+            </div>
+            <div className="content text-lg">
+              <h2 className="text-3xl suzuki-bold uppercase text-sky-600 font-bold">
+                VISION
+              </h2>
+              <p>
+                Al Rostamani Group’s vision is to be regarded as a responsible,
+                reliable and respectable business conglomerate and to be the
+                partner of choice for international and regional corporations
+              </p>
+            </div>
+            <div className="content text-lg">
+              <h2 className="text-3xl uppercase text-sky-600 font-bold">
+                COMMITMENTS
+              </h2>
+              <p>
+                True to the company Mission &amp; Vision Values Al Rostamani
+                Trading Company LLC is committed to provide a much focused
+                attitude to deliver the exceptional Customer Service Experience
+                through its network of 6 state of the art Suzuki Service and
+                Parts outlets across the emirates of UAE.
+              </p>
+              <p>
+                A variety of Services are obtainable to give your Suzuki the
+                attention it deserves to keep you on the move always without
+                facing any unforeseen breakdowns.
+              </p>
+              <p>
+                You can call us, visit our website or our Suzuki Service and
+                parts locations to know more about the services that we offer.
+              </p>
+              <p>
+                Furthermore, Al Rostamani Trading Company LLC – Suzuki is
+                committed to provide the below values to meet the high standards
+                expected by Suzuki Owners.
+              </p>
+              <ul>
+                <li className="font-bold">
+                  Quality Service
+                  <ul className="font-normal">
+                    <li>Suzuki Trained Professional Technician</li>{" "}
+                    <li>Suzuki Genuine Parts</li>{" "}
+                    <li>Suzuki prescribed Tools &amp; Equipment</li>{" "}
+                    <li>Fix it Right First Time (FRFT)</li>
+                  </ul>
+                </li>
+                <li className="font-bold">
+                  Deliver as Promised
+                  <ul className="font-normal">
+                    <li>Same Day Delivery Promise for Periodic Maintenance</li>{" "}
+                    <li>Express Maintenance with Appointments </li>
+                  </ul>
+                </li>
+                <li className="font-bold">
+                  Highly Transparent &amp; Ethical in business dealings
+                  <ul className="font-normal">
+                    <li>Competitive Service Charges </li>{" "}
+                    <li>
+                      Explanation of the Job or Invoice for all services and
+                      repairs carried out on all visits
+                    </li>
+                  </ul>
+                </li>
+                <li className="font-bold">
+                  Create WOW Experience
+                  <ul className="font-normal">
+                    <li>Active Delivery </li>{" "}
+                    <li>
+                      Complimentary Wash on All Periodic Maintenance Service
+                      Visit
+                    </li>{" "}
+                    <li>Service Reminders </li>{" "}
+                    <li>Free End of Warranty Inspection </li>
+                  </ul>
+                </li>
+                <li className="font-bold">
+                  Convenience to the Core
+                  <ul className="font-normal">
+                    <li>
+                      Drop off facility to the nearest Metro/Taxi/Bus stations
+                    </li>{" "}
+                    <li>Fully Equipped Customer Waiting Lounge </li>{" "}
+                    <li>
+                      Same Day delivery &amp; Express Service Facility with
+                      appointments
+                    </li>{" "}
+                    <li>
+                      Fleet Owners TAT (Turnaround Time) Conscious Service
+                      Program
+                    </li>
+                  </ul>
+                </li>
+                <li className="font-bold">
+                  Value for Money
+                  <ul className="font-normal">
+                    <li>Hassle Free Service Contracts with easy of Payment</li>{" "}
+                    <li>
+                      Cars maintained Periodically as prescribed by Suzuki gains
+                      enhanced resale value
+                    </li>{" "}
+                    <li>Exceptional Customer Service Experience</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
