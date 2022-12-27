b4a8039ca163953ede4d442063d3bcf607885501
rimoza <ridwan.maxamed1@gmail.com>
2022-12-27 05:18:18 -0800

    add image gallery

---
+++ app/show-room/images.ts
@@ -0,0 +1,123 @@
+import { Image } from "react-grid-gallery";
+
+export interface CustomImage extends Image {
+  original: string;
+}
+
+export const images: CustomImage[] = [
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 174,
+    // tags: [
+    //   { value: "Nature", title: "Nature" },
+    //   { value: "Flora", title: "Flora" },
+    // ],
+    caption: "After Rain (Jeshu John - designerspics.com)",
+  },
+  {
+    src: "/images/models/baleno/1.jpeg",
+    original: "/images/models/baleno/1.jpeg",
+    width: 320,
+    height: 212,
+    caption: "Boats (Jeshu John - designerspics.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    caption: "Color Pencils (Jeshu John - designerspics.com)",
+  },
+  {
+    src: "/images/models/ciaz/1.png",
+    original: "/images/models/ciaz/1.png",
+    width: 320,
+    height: 212,
+    caption: "Red Apples with other Red Fruit (foodiesfeed.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    caption: "37H (gratispgraphy.com)",
+  },
+  {
+    src: "/images/models/jimny/1.png",
+    original: "/images/models/jimny/1.png",
+    width: 320,
+    height: 212,
+    // tags: [{ value: "Nature", title: "Nature" }],
+    caption: "8H (gratisography.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    caption: "286H (gratisography.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    // tags: [{ value: "People", title: "People" }],
+    caption: "315H (gratisography.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    caption: "201H (gratisography.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    alt: "Big Ben - London",
+    width: 320,
+    height: 212,
+    caption: "Big Ben (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    alt: "Red Zone - Paris",
+    width: 320,
+    height: 212,
+    // tags: [{ value: "People", title: "People" }],
+    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    alt: "Wood Glass",
+    width: 320,
+    height: 212,
+    caption: "Wood Glass (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    caption: "A photo by 贝莉儿 NG. (unsplash.com)",
+  },
+  {
+    src: "/images/models/celerio/1.png",
+    original: "/images/models/celerio/1.png",
+    width: 320,
+    height: 212,
+    caption: "A photo by Matthew Wiebe. (unsplash.com)",
+  },
+];
