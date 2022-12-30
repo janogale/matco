8a586513669916977ea3bb06be5cf455e7fc598b
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2022-12-30 16:11:50 +0200

    feat: added more specificiation

--- app/cars/[carId]/page.tsx
+++ app/cars/[carId]/page.tsx
@@ -223,7 +223,9 @@ export default function Page({ params }: any) {
                           open
                         >
                           <summary className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50">
-                            <h2 className="font-medium text-gray-900">one</h2>
+                            <h2 className="font-medium text-gray-900">
+                              PERFORMANCE & FUEL CONSUMPTION
+                            </h2>
 
                             <svg
                               className="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
@@ -242,17 +244,18 @@ export default function Page({ params }: any) {
                           </summary>
 
                           <p className="px-4 mt-4 leading-relaxed text-gray-700">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Ab hic veritatis molestias culpa in,
-                            recusandae laboriosam neque aliquid libero nesciunt
-                            voluptate dicta quo officiis explicabo consequuntur
-                            distinctio corporis earum similique!
+                            The new Jimny's 1.5$ engine generates strong torque
+                            over a wide rpm range for powerful off-road
+                            performance. small and lightweight, it also delivers
+                            high fuel efficiency and CO2 emissions from 146g/km.
                           </p>
                         </details>
 
                         <details className="group [&_summary::-webkit-details-marker]:hidden">
                           <summary className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50">
-                            <h2 className="font-medium text-gray-900">two</h2>
+                            <h2 className="font-medium text-gray-900">
+                              STORAGE & UTILITY
+                            </h2>
 
                             <svg
                               className="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
@@ -271,11 +274,134 @@ export default function Page({ params }: any) {
                           </summary>
 
                           <p className="px-4 mt-4 leading-relaxed text-gray-700">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Ab hic veritatis molestias culpa in,
-                            recusandae laboriosam neque aliquid libero nesciunt
-                            voluptate dicta quo officiis explicabo consequuntur
-                            distinctio corporis earum similique!
+                            The new Jimny's 1.5$ engine generates strong torque
+                            over a wide rpm range for powerful off-road
+                            performance. small and lightweight, it also delivers
+                            high fuel efficiency and CO2 emissions from 146g/km.
+                          </p>
+                        </details>
+                        <details
+                          className="group [&_summary::-webkit-details-marker]:hidden"
+                          open
+                        >
+                          <summary className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50">
+                            <h2 className="font-medium text-gray-900">
+                              PERFORMANCE & FUEL CONSUMPTION
+                            </h2>
+
+                            <svg
+                              className="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M19 9l-7 7-7-7"
+                              />
+                            </svg>
+                          </summary>
+
+                          <p className="px-4 mt-4 leading-relaxed text-gray-700">
+                            The new Jimny's 1.5$ engine generates strong torque
+                            over a wide rpm range for powerful off-road
+                            performance. small and lightweight, it also delivers
+                            high fuel efficiency and CO2 emissions from 146g/km.
+                          </p>
+                        </details>
+
+                        <details className="group [&_summary::-webkit-details-marker]:hidden">
+                          <summary className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50">
+                            <h2 className="font-medium text-gray-900">
+                              STORAGE & UTILITY
+                            </h2>
+
+                            <svg
+                              className="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M19 9l-7 7-7-7"
+                              />
+                            </svg>
+                          </summary>
+
+                          <p className="px-4 mt-4 leading-relaxed text-gray-700">
+                            The new Jimny's 1.5$ engine generates strong torque
+                            over a wide rpm range for powerful off-road
+                            performance. small and lightweight, it also delivers
+                            high fuel efficiency and CO2 emissions from 146g/km.
+                          </p>
+                        </details>
+                        <details
+                          className="group [&_summary::-webkit-details-marker]:hidden"
+                          open
+                        >
+                          <summary className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50">
+                            <h2 className="font-medium text-gray-900">
+                              PERFORMANCE & FUEL CONSUMPTION
+                            </h2>
+
+                            <svg
+                              className="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M19 9l-7 7-7-7"
+                              />
+                            </svg>
+                          </summary>
+
+                          <p className="px-4 mt-4 leading-relaxed text-gray-700">
+                            The new Jimny's 1.5$ engine generates strong torque
+                            over a wide rpm range for powerful off-road
+                            performance. small and lightweight, it also delivers
+                            high fuel efficiency and CO2 emissions from 146g/km.
+                          </p>
+                        </details>
+
+                        <details className="group [&_summary::-webkit-details-marker]:hidden">
+                          <summary className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50">
+                            <h2 className="font-medium text-gray-900">
+                              STORAGE & UTILITY
+                            </h2>
+
+                            <svg
+                              className="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M19 9l-7 7-7-7"
+                              />
+                            </svg>
+                          </summary>
+
+                          <p className="px-4 mt-4 leading-relaxed text-gray-700">
+                            The new Jimny's 1.5$ engine generates strong torque
+                            over a wide rpm range for powerful off-road
+                            performance. small and lightweight, it also delivers
+                            high fuel efficiency and CO2 emissions from 146g/km.
                           </p>
                         </details>
                       </div>
