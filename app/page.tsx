269410b87997e46bd3b5b45f776af940400e747a
rimoza <ridwan.maxamed1@gmail.com>
2023-01-02 01:13:33 -0800

    Minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-import { ThemeProvider } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-
 import Banner from "../components/ui/Banner";
 import HomeCarousel from "../components/HomeCarousel";
 import TabsCars from "../components/TabsCars";
 import Container from "../components/ui/Container";
 import Heading from "../components/ui/Heading";
 import HomeCTA from "../components/HomeCTA";
-import theme from "./theme";
+
 export default function Home() {
   return (
     <main>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
         <HomeCarousel />
         <Container>
           <Banner text1="Explore the Affordable" text2="New Models" />
@@ -23,7 +18,6 @@ export default function Home() {
         </Container>
         <HomeCTA />
         <div className="my-16"></div>
-      </ThemeProvider>
     </main>
   );
 }
