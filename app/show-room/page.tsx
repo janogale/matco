46efde938a271d9edef445a3b9a7d6989c691a63
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2023-01-05 10:33:17 +0300

    Refactor show-room

--- app/show-room/page.tsx
+++ app/show-room/page.tsx
@@ -1,47 +1,53 @@
 "use client";
 
 import { useState } from "react";
-
 import PhotoAlbum from "react-photo-album";
-
+// @ts-ignore
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
 // import optional lightbox plugins
+// @ts-ignore
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+// @ts-ignore
 import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+// @ts-ignore
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+// @ts-ignore
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
-import photos from "./images";
-import slides from "./slides";
 import Container from "../../components/ui/Container";
 
+import gallery from "../../sampledata/gallery";
 
 const ShowRoom = () => {
-  const [index, setIndex] = useState(-1);
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
 
   return (
     <Container>
-      <div className="main">
+      <div className="mt-12 min-h-[500px]">
         <PhotoAlbum
-          photos={photos}
+          photos={gallery}
           layout="rows"
           targetRowHeight={150}
-          onClick={(index) => setIndex(1)}
+          onClick={(obj) => {
+            setIndex(obj?.index);
+            setOpen(true);
+          }}
         />
       </div>
 
       <Lightbox
-       open={index >= 0}
-       index={index}
-       close={() => setIndex(-1)}
-       slides={slides}
-       plugins={[Fullscreen, Slideshow, Thumbnails]}
-       thumbnails={{
-         borderRadius: 10,
-       }}
+        open={open}
+        close={() => setOpen(false)}
+        index={index}
+        slides={gallery}
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+        thumbnails={{
+          borderRadius: 10,
+        }}
       />
     </Container>
   );
