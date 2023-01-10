516784465929541a0fd979f97c48f2b62aa3e7f7
rimoza <ridwan.maxamed1@gmail.com>
2023-01-09 23:59:28 -0800

    Minor style change at accessories page

--- components/AccessoriesItem.tsx
+++ components/AccessoriesItem.tsx
@@ -9,19 +9,24 @@ export const AccessoriesItem = ({
 }) => {
   return (
     <Card
-        className="max-w-sm transition-transform duration-500 transform ease-in-out hover:scale-105 shadow-none border-none hover:shadow-md"
-        imgAlt="Meaningful alt text for an image that is not purely decorative"
-        imgSrc={accessoriesItem.image}
-      >
-    <div className="">
-        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {accessoriesItem.name}
+      className="max-w-sm transition-transform duration-500 transform ease-in-out hover:scale-105 shadow-none border-none hover:shadow-md"
+      imgAlt="Meaningful alt text for an image that is not purely decorative"
+      imgSrc={accessoriesItem.image}
+    >
+      <div>
+        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white mb-2">
+          {accessoriesItem.name.toUpperCase()}
         </h5>
-        <p className="text-sm mt-1"><span className="font-bold">Part No:</span> {accessoriesItem.partNumber}</p>
-        <p className="mt-5 mb-2 text-base text-gray-500 dark:text-gray-400 sm:text-md">
+        <p className="my-3 mb-2 text-base text-gray-500 dark:text-gray-400 sm:text-md">
           {accessoriesItem.description}
         </p>
-    </div>
-      </Card>
+        <hr />
+        <p className="text-sm mt-1 my-2">
+          <span className="font-bold mr-10">PART NO:</span>
+          <span>{accessoriesItem.partNumber}</span>
+        </p>
+        <hr />
+      </div>
+    </Card>
   );
 };
