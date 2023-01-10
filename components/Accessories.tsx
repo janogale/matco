20ba0c9dc94e311ca0b2b9d11418a35604ba9d88
rimoza <ridwan.maxamed1@gmail.com>
2023-01-10 02:21:40 -0800

    Style fixes

--- components/Accessories.tsx
+++ components/Accessories.tsx
@@ -8,7 +8,7 @@ export const Accessories = ({
 }) => {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center gap-5 my-16 cursor-pointer">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-y-12 gap-x-5 my-16 cursor-pointer">
         {accessories.map((accessoriesItem: IAccessoriesType) => {
           return (
             <AccessoriesItem
--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Container({ children }: any) {
+export default function Container({ children, className }: { children: React.ReactNode, className?: string }) {
   return (
-    <section className={`flex justify-center items-center`}>
+    <section className={`flex justify-center items-center ${className}`}>
       <div className="max-w-6xl flex-1">{children}</div>
     </section>
   );
