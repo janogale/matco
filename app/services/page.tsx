0175e17e3c82d3448645bb1cb5ed77917d0b8643
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2023-01-21 10:18:52 +0300

    Create service Page/route

---
+++ app/services/page.tsx
@@ -0,0 +1,14 @@
+import Container from "../../components/ui/Container";
+import Heading from "../../components/ui/Heading";
+
+export default function ServicesPage() {
+  return (
+    <Container>
+      <div className="relative bg-white py-16">
+        <Heading className="text-center text-red-600">
+          Services Page is Under Development!
+        </Heading>
+      </div>
+    </Container>
+  );
+}
