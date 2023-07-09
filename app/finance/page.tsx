e5ac2992cce017a6dba514f491f7b2026ea76e3c
rimoza <ridwan.maxamed1@gmail.com>
2023-07-09 18:27:46 +0300

    Add finance page

---
+++ app/finance/page.tsx
@@ -0,0 +1,14 @@
+import Container from "../../components/ui/Container";
+import Heading from "../../components/ui/Heading";
+
+export default function FinancePage() {
+  return (
+    <Container>
+      <div className="relative bg-white py-16">
+        <Heading className="text-center text-red-600">
+          Finance Page is Under Development!
+        </Heading>
+      </div>
+    </Container>
+  );
+}
