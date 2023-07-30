8911c0db4283fb01c35d46b4b78f4c0ad4fb4e21
rimoza <ridwan.maxamed1@gmail.com>
2023-07-30 10:27:35 +0300

    Add responsive style ad jac discover page

--- app/discover/jac/page.tsx
+++ app/discover/jac/page.tsx
@@ -9,17 +9,17 @@ import ImageBanner from "../../../public/images/jac-discover/about-banner.webp";
 const JacDiscover = () => {
   return (
     <Container>
-      <div className="rounded-lg overflow-hidden w-full h-96 min-h-96">
+      <div className="rounded-lg overflow-hidden w-full h-96 md:min-h-96">
         <Image
           src={ImageBanner}
           alt="cover"
           width={500}
           height={500}
-          className="w-full h-auto group text-center rounded-md bg-white hover:cursor-pointer hover:opacity-75 hover:scale-105 transform transition ease-out duration-500 cursor-pointer"
+          className="w-full h-full group text-center rounded-md bg-white hover:cursor-pointer hover:opacity-75 hover:scale-105 transform transition ease-out duration-500 cursor-pointer"
         />
       </div>
       <Banner text1="" text2="Better Drive. Better Life" />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 px-5 md:px-1 lg:px-0">
         <div className="text-gray-500">
           <p>
             JAC is a comprehensive auto-maker with full-line independent brand
@@ -53,11 +53,11 @@ const JacDiscover = () => {
           />
         </div>
       </div>
-      <div className="my-10 rounded-lg overflow-hidden w-full bg-red-500">
+      <div className="my-10 rounded-lg overflow-hidden px-5 md:px-1 lg:px-0">
         <div>
         <iframe
-          width="1280"
-          height="720"
+          width="100%"
+          height="630"
           src="https://www.youtube.com/embed/I-xdBMPN5WM"
           title="JAC Hefei R&amp;D Center"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
