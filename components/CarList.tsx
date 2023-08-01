f2ab11c64fbdf5941c80dd595bdbb3d7b4a9b2e3
rimoza <ridwan.maxamed1@gmail.com>
2023-08-01 10:59:45 +0300

    Fix error

--- components/CarList.tsx
+++ components/CarList.tsx
@@ -1,17 +1,12 @@
 import ImageCard from "./ui/ImageCard";
 
 export type TCar = {
-  id: number | string;
-  href: string;
-  image: string;
+  id: number;
   name: string;
-  price: string;
-  modalYear: string;
-  km: string;
-  fuelType: string;
-  carType: string;
-  engineCapacity: string;
   description: string;
+  coverImage: string;
+  images: string[];
+  features: string[];
   logo?: string;
 };
 
@@ -21,7 +16,7 @@ export default function CarList({ cars }: { cars: TCar[] }) {
       <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 ">
         {cars.map((car) => (
           <div key={car.id}>
-            <ImageCard img={car.image} logo={car.logo} alt={car.name} />
+            <ImageCard img={car.coverImage} logo={car.logo} alt={car.name} />
           </div>
         ))}
       </div>
