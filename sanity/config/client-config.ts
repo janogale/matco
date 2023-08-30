d77a4080fea1fa6b72c3b79f2bbeb4a51c04c283
rimoza <ridwan.maxamed1@gmail.com>
2023-08-30 17:52:29 +0300

    Add .env file

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,7 +1,11 @@
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+
 const config = {
-    projectId: "a8x3s50m",
-    dataset: "production",
-    apiVersion: "2023-08-30",
-}
+  projectId,
+  dataset,
+  apiVersion,
+};
 
 export default config;
