e3dc427f4ec9ec0c159f19d11f71cec8f8b26e06
rimoza <ridwan.maxamed1@gmail.com>
2023-09-03 19:29:01 +0300

    Minor change

--- components/ui/Spinner.tsx
+++ components/ui/Spinner.tsx
@@ -8,7 +8,7 @@ export default function Buttons() {
   return (
     <div className="flex justify-center items-center">
       <Spinner aria-label="Alternate spinner button example" />
-      <span className="pl-3 text-lg text-gray-400 font-semibold">Something magic is going on</span>
+      <span className="pl-3 text-lg text-gray-400 font-semibold">Please wait a little bit</span>
     </div>
   );
 }
